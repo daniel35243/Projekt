c8d9ab8e1693010ba00a8b4c373590c0842e3cf5
Tobi881 <tobizockt3244@gmail.com>
2025-05-02 15:18:04 +0200

    Start Screen ohne erscheinen

---
+++ spiel/core/src/main/java/io/gihub/FarmLifeSplash/Splash.java
@@ -0,0 +1,60 @@
+package io.gihub.FarmLifeSplash;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Splash implements Screen {
+
+    private Sprite splash;
+    private SpriteBatch batch;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+
+        Texture splashTexture = new Texture("FarmLifeLogo.png");
+        splash = new Sprite(splashTexture);
+        splash.setSize(1000, 1000);
+        splash.setPosition(650, 0);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        splash.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
