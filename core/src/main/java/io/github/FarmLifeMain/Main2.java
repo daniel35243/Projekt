883b9425ceaaf36021bd5271cd8868b06d8692e4
Tobias H <tobizockt324@gmail.com>
2025-05-07 01:06:01 +0200

    Geht nicht main Menu

---
+++ core/src/main/java/io/github/FarmLifeMain/Main2.java
@@ -0,0 +1,10 @@
+package io.github.FarmLifeMain;
+
+import com.badlogic.gdx.Game;
+
+public class Main2 extends Game {
+    @Override
+    public void create() {
+        setScreen(new Splash(this));
+    }
+}
